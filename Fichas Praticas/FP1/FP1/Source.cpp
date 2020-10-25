f2c9455c16faef64862174ad4536af383e24b6b7
Afonso Correia <a47521@alunos.isel.pt>
2020-10-25 14:52:25 +0000

    FIXED

--- Fichas Praticas/FP1/FP1/Source.cpp
+++ Fichas Praticas/FP1/FP1/Source.cpp
@@ -1,11 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-void main()
+
+int main()
 {
 	FILE* pfile; // Apontador para uma estrutura de um ficheiro
-	char nome_ficheiro[] = { "c:\\Teste.txt" }; // String com o nome do ficheiro
-	float* valores; // Apontador para o Array dinâmico que guarda os valores // lidos do ficheiro
+	char nome_ficheiro[] = { "teste.txt" }; // String com o nome do ficheiro
+	int* valores; // Apontador para o Array dinâmico que guarda os valores // lidos do ficheiro
 	int nlinhas; // Nº de dados que o ficheiro contém
 	int nclasses; // Nº de classes do histograma
+	int i = 0;
 	///////////////////////////////////////////////////////////
 	// Leitura dos dados de um ficheiro para o array valores
 	//
@@ -17,12 +20,17 @@ void main()
 	{
 		// Leitura da 1ª linha com o nº de classes do histograma
 		fscanf(pfile, "%d", &nclasses);
+		printf("%d\n", nclasses);
 		// Leitura da 2ª linha com o total de valores a ler
 		fscanf(pfile, "%d", &nlinhas);
-		valores = new float[nlinhas];
+		printf("%d\n", nlinhas);
+		valores = new int[nlinhas];
 		// Leitura das linhas seguintes
-		for (i = 0; i < nlinhas; i++)
-			fscanf(pfile, "%f", &valores[i]);
-		///////////////////////////////////////////////////////////
-		// C O M P L E T A R
+		for (i; i < nlinhas; i++) {
+			fscanf(pfile, "%d", &valores[i]);
+			printf("%d, ", valores[i]);
+		}
+		
 		delete[] valores; // Libertação da memória ocupada pelo array dinâmico
+	}
+}
