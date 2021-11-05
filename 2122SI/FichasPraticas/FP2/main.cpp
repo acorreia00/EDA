e65889e915c91ce70f0763a992218ca8f57abcef
acorreia00 <afonsoscorreia@gmail.com>
2021-11-05 14:15:29 +0000

    FP2 :(

---
+++ 2122SI/FichasPraticas/FP2/main.cpp
@@ -0,0 +1,82 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "matriz.h"
+
+void main()
+{
+	char ficheiro_matriz_a[] = { "C:\\Users\\Diogo\\OneDrive\\Ambiente de trabalho\\matriz_1.txt" };
+	char ficheiro_matriz_b[] = { "C:\\Users\\Diogo\\OneDrive\\Ambiente de trabalho\\matriz_2.txt" };
+	char ficheiro_matriz_escrita[] = { "C:\\Users\\Diogo\\OneDrive\\Ambiente de trabalho\\matriz.txt" };
+
+	Matrix matriz_a, matriz_b, matriz_c;
+
+	if (!matriz_a.ReadFromFile(ficheiro_matriz_a))
+	{
+		printf("Falhou a leitura da matriz A.\n");
+		system("pause");
+		exit(1);
+	}
+	if (!matriz_b.ReadFromFile(ficheiro_matriz_b))
+	{
+		printf("Falhou a leitura da matriz B.\n");
+		system("pause");
+		exit(1);
+	}
+
+	//Teste de Leitura e Impressao
+	printf("Leitura das Matrizes apartir de ficheiros:\nMatriz A:\n");
+	matriz_a.Output();
+	printf("Matriz B:\n");
+	matriz_b.Output();
+	//system("pause");
+
+/*	//Teste de Atribuicao, Soma e Multiplicacao;
+	printf("Matriz C = Matriz A:\nMatriz C Antes:\n");
+	matriz_c = Matrix(2, 2);
+	matriz_c.Output();
+	matriz_c = matriz_a;
+	printf("Matriz C Depois:\n");
+	matriz_c.Output();
+
+	printf("Matriz A + Matriz B:\n");
+	matriz_c = matriz_a + matriz_b;
+	matriz_c.Output();
+	printf("Matriz A * Matriz B:\n");
+	matriz_c = matriz_a * matriz_b;
+	matriz_c.Output();
+	matriz_c.SaveToFile(ficheiro_matriz_escrita);
+	printf("Resultado foi escrito no ficheiro \n");
+	//system("pause");*/
+
+	matriz_b = matriz_a.GetLU();
+	printf("Teste LU\n");
+	matriz_b.Output();
+
+	matriz_a.GetDeterminant();
+	printf("teste determinante");
+	matriz_a.Output();
+	/*//Teste de += e *=
+    matriz_c = matriz_a;
+    printf("Teste +=:\n");
+    matriz_c.Output();
+    matriz_c += 2; 
+    matriz_c.Output();
+    //system("pause");
+
+    matriz_c = matriz_a;
+    printf("Teste *=:\n");
+    matriz_c.Output();
+    matriz_c *= 2; 
+    matriz_c.Output();
+    //system("pause");
+
+    //Teste da matriz transposta
+    matriz_c = matriz_a;
+    printf("Teste da matriz transposta:\n");
+    matriz_c.Output();
+    matriz_c.Transpose();
+    matriz_c.Output();
+    //system("pause");*/
+}
---
+++ 2122SI/FichasPraticas/FP2/matriz.cpp
@@ -0,0 +1,259 @@
+#define _SRT_SECURE_NO_WARNINGS
+#include "matriz.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void Matrix::Delete()
+{
+	for (int i = 0; i < nLines; i++)
+		delete[]elems[i];
+	delete[]elems;
+}
+
+void Matrix::Create(int lines, int cols)
+{
+	nLines = lines;
+	nCols = cols;
+	elems = new float * [nLines];
+	for (int i = 0; i < nLines; i++)
+	{
+		elems[i] = new float [nCols];
+		for (int j = 0; j < nCols; j++)
+		{
+			elems[i][j] = 0;
+		}
+	}
+}
+
+Matrix::Matrix()
+{
+	elems = NULL;
+	nLines = 0;
+	nCols = 0;
+}
+
+Matrix::Matrix(const Matrix& m1)
+{
+	Create(m1.nLines, m1.nCols);
+	for (int i = 0; i < nLines; i++)
+	{
+		for (int j = 0; j < nCols; j++)
+		{
+			elems[i][j] = m1.elems[i][j];
+		}
+	}
+}
+
+Matrix::Matrix(int lines, int cols)
+{
+	Create(lines, cols);
+}
+
+Matrix::~Matrix()
+{
+	Delete();
+}
+
+bool Matrix::CanMultiply(const Matrix* pm)
+{
+	return nCols == pm->nLines;
+}
+
+bool Matrix::CanAdd(const Matrix* pm)
+{
+	return nCols == pm->nCols && nLines == pm->nLines;
+}
+
+const Matrix& Matrix::operator=(const Matrix& m1)
+{
+	Create(m1.nLines, m1.nCols);
+	for (int i = 0; i < nLines; i++)
+	{
+		for (int j = 0; j < nCols; j++)
+		{
+			elems[i][j] = m1.elems[i][j];
+		}
+	}
+
+	return *this;
+}
+
+Matrix Matrix::operator+(const Matrix& m1)
+{
+	Matrix result(m1.nLines, m1.nCols);
+	if (CanAdd(&m1))
+	{
+		for (int i = 0; i < nLines; i++)
+		{
+			for (int j = 0; j < nCols; j++)
+			{
+				result.elems[i][j] = elems[i][j] + m1.elems[i][j];
+			}
+		}
+	}
+	else
+	{
+		printf("\nA soma nao eh possivel");
+	}
+
+	return result;
+}
+
+Matrix Matrix::operator*(const Matrix& m1)
+{
+        Matrix result(nCols, m1.nLines);
+		if (CanMultiply(&m1)) 
+		{
+			for (int i = 0; i < result.nLines; i++)
+			{
+				for (int j = 0; j < result.nCols; j++)
+				{
+					for (int k = 0; k < result.nCols; k++)
+					{
+						result.elems[i][j] += elems[i][k] * m1.elems[k][j];
+					}
+				}
+			}
+		}
+		else
+		{
+			printf("\nO produto nao eh possivel");
+		}
+		return result;
+}
+
+const Matrix& Matrix::operator+=(int k)
+{
+	for (int i = 0; i < nLines; i++)
+	{
+		for (int j = 0; j < nCols; j++)
+		{
+			elems[i][j] = elems[i][j] + k;
+		}
+	}
+	return *this;
+}
+
+const Matrix& Matrix::operator*=(int k)
+{
+	for (int i = 0; i < nLines; i++)
+	{
+		for (int j = 0; j < nCols; j++)
+		{
+			elems[i][j] = elems[i][j] * k;
+		}
+	}
+	return *this;
+}
+
+void Matrix::Transpose()
+{
+	Matrix temp;
+	temp = *this;
+	Delete();
+	Create(temp.nCols, temp.nLines);
+	for (int i = 0; i < nLines; i++)
+	{
+		for (int j = 0; j < nCols; j++)
+		{
+			elems[i][j] = temp.elems[j][i];
+		}
+	}
+}
+
+bool Matrix::ReadFromFile(char* file_name)
+{
+	FILE* file;
+	fopen_s(&file, file_name, "r");
+	if (!file) return false;
+	fscanf_s(file, "%d", &nLines);
+	fscanf_s(file, "%d", &nCols);
+	Create(nLines, nCols);
+	for (int i = 0; i < nLines; i++)
+	{
+		for (int j = 0; j < nCols; j++)
+			fscanf_s(file, "%f", &elems[i][j]);
+	}
+	fclose(file);
+	return true;
+}
+
+bool Matrix::SaveToFile(char* file_name)
+{
+	FILE* file;
+	fopen_s(&file, file_name, "w");
+	if (!file) return false;
+	fprintf_s(file, "%d %d \n", nLines, nCols);
+	for (int i = 0; i < nLines; i++)
+	{
+		for (int j = 0; j < nCols; j++)
+			fprintf_s(file, "%d \n", elems[i][j]);
+	}
+	fclose(file);
+	return true;
+}
+
+void Matrix::Output()
+{
+	for (int i = 0; i < nLines; i++)
+	{
+		for (int j = 0; j < nCols; j++)
+		{
+			printf("%g  ", elems[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+Matrix Matrix::GetLU()
+{
+	Matrix U;
+	U = *this;
+	float f;
+	//Create(U.nCols, U.nLines);
+	for (int i = 0;i <= nLines - 2;i++) {
+		for (int j = i + 1;j <= nLines - 1;j++) {
+			//elems[j][i] = U.elems[j][i] / U.elems[i][i];
+			f = U.elems[j][i] / U.elems[i][i];
+			for (int m = i + 1;m < nCols;m++) {
+				U.elems[j][m] = U.elems[j][m] - U.elems[i][m] * f;
+			}
+			U.elems[j][i]=f;
+		}
+	}
+	return U;
+}
+
+float Matrix::GetDeterminant(const Matrix& m1)
+{
+	Matrix D(m1.nLines, m1.nCols);
+	float f = 0;
+	if (CanAdd(&m1))
+	{
+		for (int i = 0; i <= nLines;i++)
+		{
+			f += D.elems[i][i];
+		}
+
+		return f;
+	}
+	else
+	{
+		printf("A matriz nao eh quadrada");
+		return 0;
+	}
+}
+/*Matrix Matrix::GetInverse(const Matrix& m1)
+{
+	if ()
+	{
+		Matrix I;
+		I = *this;
+		return I;
+	}
+	else 
+	{
+
+	}
+}*/
